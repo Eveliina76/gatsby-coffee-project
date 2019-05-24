bfea0ceca5331dba035bf891c8caf9ea13408d91
Eveliina Champagne <eveliina.champagne@wunder.io>
2019-05-24 14:49:38 +0300

    Remove the second shopping card from the site

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,14 +2,12 @@ import React from "react"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
-import { FaCartArrowDown } from "react-icons/fa"
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
     <h1>Perfect blend Coffee House</h1>
     <p>Come and enjoy our awesome coffees</p>
-    <FaCartArrowDown />
   </Layout>
 )
 
