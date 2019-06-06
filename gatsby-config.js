f208168a82ec39e55e3f03408f6ba76c27a3f36d
Eveliina Champagne <eveliina.champagne@wunder.io>
2019-06-06 16:12:13 +0300

    trying fixes

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,10 @@
-require("dotenv").config({
-  path: `.env.${process.env.NODE_ENV}`,
-})
+const dotenv = require("dotenv")
+
+if (process.env.ENVIRONMENT !== "production") {
+  dotenv.config()
+}
+
+const { spaceId, accessToken } = process.env
 
 module.exports = {
   siteMetadata: {
