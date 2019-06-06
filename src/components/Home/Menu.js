0002c76939c1f4a0a3812c8448409ea2cb56aea1
Eveliina Champagne <eveliina.champagne@wunder.io>
2019-06-06 10:38:26 +0300

    Add Menu, index and data from contentfull

---
+++ src/components/Home/Menu.js
@@ -0,0 +1,62 @@
+import React, { Component } from "react"
+import Title from "../Globals/Title"
+import Img from "gatsby-image"
+
+export default class Menu extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      items: props.items.edges,
+      coffeeItems: props.items.edges,
+    }
+  }
+  render() {
+    if (this.state.items.length > 0) {
+      return (
+        <section className="menu py-5">
+          <div className="container">
+            <Title title="our coffees" />
+            {/* categories */}
+            {/* items */}
+            <div className="row">
+              {this.state.coffeeItems.map(({ node }) => {
+                return (
+                  <div
+                    key={node.id}
+                    className="col-11 col-md-6 my-3 d-flex mx-auto"
+                  >
+                    <div>
+                      <Img fixed={node.image.fixed} />
+                    </div>
+                    {/* item text */}
+                    <div className="flex-grow-1 px-3">
+                      <div className="d-flex justify-content-between">
+                        <h6 className="mb-0">{node.title}</h6>
+                        <h6 className="mb-0">€{node.price}</h6>
+                      </div>
+                      <p className="text-muted">
+                        <small>{node.description.description}</small>
+                      </p>
+                    </div>
+                  </div>
+                )
+              })}
+            </div>
+          </div>
+        </section>
+      )
+    } else {
+      return (
+        <section className="menu py-5 ">
+          <div className="container">
+            <Title title="our coffees" />
+            <div className="row">
+              <div className="col-10 col-sm-6 mx-auto text-center text-capitalize" />
+              <h1>There are no items to display</h1>
+            </div>
+          </div>
+        </section>
+      )
+    }
+  }
+}
