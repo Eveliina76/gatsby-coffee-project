8de759d5aa229ff8629a49ce79ce5665ae00919d
Eveliina Champagne <eveliina.champagne@wunder.io>
2019-06-06 14:01:23 +0300

    fix

--- gatsby-config.js
+++ gatsby-config.js
@@ -20,8 +20,8 @@ module.exports = {
     {
       resolve: `gatsby-source-contentful`,
       options: {
-        spaceId: process.env.CONTENTFUL_SPACE_ID,
-        accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
+        spaceId,
+        accessToken,
       },
     },
 
