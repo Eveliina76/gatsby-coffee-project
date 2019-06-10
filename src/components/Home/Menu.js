856d25dc98c9fe7a880db7e658fee26be1d92754
Eveliina Champagne <eveliina.champagne@wunder.io>
2019-06-10 16:16:39 +0300

    Adding categories to Menu, changing images back to images folder

--- src/components/Home/Menu.js
+++ src/components/Home/Menu.js
@@ -2,12 +2,22 @@ import React, { Component } from "react"
 import Title from "../Globals/Title"
 import Img from "gatsby-image"
 
+const getCategories = items => {
+  let tempItems = items.map(items => {
+    return items.node.category
+  })
+  let tempCategories = new Set(tempItems)
+  let categories = Array.from(tempCategories)
+  categories = ["all", ...categories]
+  return categories
+}
 export default class Menu extends Component {
   constructor(props) {
     super(props)
     this.state = {
       items: props.items.edges,
       coffeeItems: props.items.edges,
+      categories: getCategories(props.items.edges),
     }
   }
   render() {
